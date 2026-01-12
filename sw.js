0b3e4603a2c1abca672e6511483e6b1c51298572
Nandish Jha <nandish.d.jha@gmail.com>
2026-01-11 21:05:00 -0600

    Add web manifest and service worker (PWA basics)

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'np-v0.1';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
